bf91ca31a4d8ecf7fcc309abe10d4053c3bf46b5
Aleksandra <>
2020-06-09 11:10:32 +0200

    Last change in Chapter 1

--- Chapter_1.py
+++ Chapter_1.py
@@ -1,13 +1,26 @@
+class IllegalCarError(Exception):
+    def __init__(self, kind):
+        self.kind = kind
+
+    def __str__(self):
+        return '{}'.format(self.kind)
+
 class Car():
     def __init__(self, pax_count, car_mass, gear_count):
         self.pax_count = pax_count
         self.car_mass = car_mass
         self.gear_count = gear_count
 
-        if not pax_count in range(1,5):
-            raise IllegalCarError("Pax count nust be greater than 1 and less than 5!")
-        if car_mass >2000:
-            raise ValueError("Car mass cannot be greater than 2000kg.")
+        try:
+            pax_count in range(1,5)
+            raise IllegalCarError('Pax count nust be greater than 1 and less than 5!')
+        except IllegalCarError as e:
+            print('Error: {}'.format(e))
+        try:
+            car_mass <= 2000
+            raise IllegalCarError('Car mass cannot be greater than 2000kg.')
+        except IllegalCarError as e:
+            print('Error: {}'.format(e))
 
         self.total_mass = pax_count * 70 + car_mass
 
